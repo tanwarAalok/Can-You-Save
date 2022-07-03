a2c59a64c78ff301e9fc2b5bc550c645bea2e1f8
Yash Goel <89704419+GoelYash1@users.noreply.github.com>
2022-07-03 19:20:01 +0530

    Some changes in Player controller

--- Game1/Assets/Scripts/PlayerController.cs
+++ Game1/Assets/Scripts/PlayerController.cs
@@ -5,32 +5,25 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     private Rigidbody2D body;
-    [SerializeField] private float speed = 5;
+    [SerializeField] private float runSpeed = 5;
     [SerializeField] private float jumpSpeed = 10;
-    private BoxCollider2D boxCollider;
-    [SerializeField] private LayerMask groundLayer;
+    private CapsuleCollider2D bodyCollider;
+    BoxCollider2D feetCollider;
 
     private void Awake() 
     {
         body = GetComponent<Rigidbody2D>();
-        boxCollider = GetComponent<BoxCollider2D>();
+        bodyCollider = GetComponent<CapsuleCollider2D>();
+        feetCollider = GetComponent<BoxCollider2D>();
     }
 
     private void Update() 
     {
-        body.velocity = new Vector2(Input.GetAxis("Horizontal") * speed, body.velocity.y);
+        body.velocity = new Vector2(Input.GetAxis("Horizontal") * runSpeed, body.velocity.y);
 
-        if(isGrounded() && Input.GetKey(KeyCode.Space))
+        if (feetCollider.IsTouchingLayers(LayerMask.GetMask("groundLayer")) && Input.GetKey(KeyCode.Space))
         {
             body.velocity = new Vector2(body.velocity.x, jumpSpeed);
         }
     }
-
-    private bool isGrounded()
-    {
-        RaycastHit2D raycastHit = Physics2D.BoxCast(boxCollider.bounds.center, boxCollider.bounds.size, 0, Vector2.down, 0.1f, groundLayer);
-        return raycastHit.collider != null;
-    }
-
-
 }
